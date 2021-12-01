2f89f5e939bfa0649be1b0871db071dbbd8d1cb4
rakesh <gontlarakesh@gmail.com>
2021-12-01 07:56:55 +0000

    About Route tests

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const NotFound = () => (
+  <>
+    <div className="notfound-content">
+      <h1 className="notfound-heading">Not Found</h1>
+    </div>
+  </>
+)
+
+export default NotFound
